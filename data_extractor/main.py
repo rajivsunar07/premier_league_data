dda31886a3441caf2a2ee7c50120b180d87ca673
rajivsunar07 <rajivsunar07@gmail.com>
2022-09-12 15:02:55 +0545

    cleaned files

--- data_extractor/main.py
+++ data_extractor/main.py
@@ -21,37 +21,17 @@ async def get_data(api, endpoints, destination_dir):
 
         responses = await asyncio.gather(*tasks)
         for res in responses:
-            # print(await res.json())
+            # Saves file according to the endpoint of the api
             file_name = str(res.url).split('/api/')[1].replace('/', '_')[:-1]
             with open("{}/{}.json".format(destination_dir, file_name), "w") as f:
                 json.dump(await res.json(), f)
                 logging.info("Data loaded into: {}.json".format(file_name))
 
-with open("config.json", "r") as f:
-    config = json.load(f)
-    api = config['api']
-    destination_dir = config['destination_dir']
-    endpoints = config['endpoints']
+if __name__ == "__main__":
+    with open("config.json", "r") as f:
+        config = json.load(f)
+        api = config['api']
+        destination_dir = config['destination_dir']
+        endpoints = config['endpoints']
 
-asyncio.run(get_data(api, endpoints, destination_dir))
-
-
-# def extract(uri, data_dir):
-#     # Loading configuration data
-#     with open("config.json", "r") as f:
-#         config = json.load(f)
-#     api = config['api']
-#     destination_dir = config['destination_dir']
-#     endpoints = config['endpoints']
-
-#     for i in endpoints:
-#         try:
-#             response = requests.get(uri)
-#             res_text = json.loads(response.text)
-#             with open(data_dir, 'w') as f:
-#                 json.dump(res_text, f)
-#         except Exception as e:
-#             logging.error(e)
-
-# if __name__ == "__main__":
-#     extract()
+    asyncio.run(get_data(api, endpoints, destination_dir))
