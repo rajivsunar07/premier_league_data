c7d20171e3be03ee31411ca74938ba46d81daf92
rajivsunar <rajivsunar07@gmail.com>
2022-10-10 21:12:56 +0545

    transformation changed

--- data_extractor/transform.py
+++ data_extractor/transform.py
@@ -1,4 +1,7 @@
 import json
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
 
 destination_dir = ''
 with open('config.json') as f:
@@ -13,5 +16,13 @@ with open(destination_dir + '/bootstrap-static.json') as f:
 
 for i in data:
     with open("{}/{}.json".format(destination_dir, i), "w") as f:
-            json.dump(data[i], f)
-            
+        json.dump(data[i], f)
+        logging.info("Data loaded into: {}.json".format(i))
+
+with open(destination_dir + '/fixtures.json', 'r') as f:
+    data = json.load(f)
+    stats  = [{'code': x['code'], 'stats': x['stats']} for x in data]
+
+with open(destination_dir + '/fixtures_stats.json', 'w') as f:
+    json.dump(stats, f)
+    logging.info("data loaded into: {}.json".format('fixtures_stats.json'))
